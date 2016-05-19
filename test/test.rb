3b2349c06c56e7a0b9f748f2abac58109e2f48e4
Sille Van Landschoot <info@sillevl.be>
2016-05-19 10:22:33 +0200

    update test file and pack data to send into bytes

--- test/test.rb
+++ test/test.rb
@@ -1,8 +1,27 @@
-
-require '../m3pi'
-
-
-robot = M3pi::Robot.new
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'm3pi'
+require 'io/console'
 
 
-robot.drive 100
+robot = M3pi::Robot.new '/dev/ttyUSB1'
+
+# 20.times do
+#   robot.drive 100, 0
+#   sleep 0.075
+# end
+
+
+while key = STDIN.getch
+  case key
+  when 'z' #forward
+    robot.drive 100, 0
+  when 'q' #left
+    robot.drive 0, -100
+  when 's' #back
+    robot.drive -100, 0
+  when 'd' #right
+    robot.drive 0, 100
+  when 'x' # quit
+    break
+  end
+end
